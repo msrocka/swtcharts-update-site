b35f9b5e004df987a269668323e66e783feb5d68
msrocka <michael.srocka@gmail.com>
2018-06-16 19:11:47 +0200

    initial commit

---
+++ bin/install.bat
@@ -0,0 +1,13 @@
+start mvn install:install-file ^
+  -Dfile=org.swtchart_0.10.0.v20160212.jar ^
+  -DgroupId=org.swtchart ^
+  -DartifactId=swtchart ^
+  -Dversion=0.10.0 ^
+  -Dpackaging=jar
+
+start mvn install:install-file ^
+  -Dfile=org.swtchart.ext_0.10.0.v20160212.jar ^
+  -DgroupId=org.swtchart ^
+  -DartifactId=swtchart-ext ^
+  -Dversion=0.10.0 ^
+  -Dpackaging=jar
